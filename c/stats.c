4339b856d85de4c949ef7c4e66ec679512809a58
Andy Keep <akeep@cisco.com>
2016-05-15 00:16:15 -0400

    - updated the fix to S_mktime to work on windows.  the struct tm   structure on windows does not have the tm_gmtoff field used in the   mac and linux version of the code.     stats.c

--- c/stats.c
+++ c/stats.c
@@ -406,7 +406,26 @@ ptr S_mktime(ptr dtvec) {
   INITVECTIT(dtvec, dtvec_year) = Sinteger(tmx.tm_year);
   INITVECTIT(dtvec, dtvec_wday) = Sinteger(tmx.tm_wday);
   INITVECTIT(dtvec, dtvec_yday) = Sinteger(tmx.tm_yday);
+#ifdef WIN32
+  {
+    TIME_ZONE_INFORMATION tz;
+    DWORD rc = GetTimeZoneInformation(&tz);
+    long tzoff;
+
+    switch (rc) {
+      case TIME_ZONE_ID_UNKNOWN:
+      case TIME_ZONE_ID_STANDARD:
+        tzoff = tz.Bias * -60;
+        break;
+      case TIME_ZONE_ID_DAYLIGHT:
+        tzoff = (tz.Bias + tz.DaylightBias) * -60;
+        break;
+    }
+    if (tzoff != orig_tzoff) tx = (time_t) difftime(tx, (time_t)(orig_tzoff - tzoff));
+  }
+#else
   if (tmx.tm_gmtoff != orig_tzoff) tx = difftime(tx, (time_t)(orig_tzoff - tmx.tm_gmtoff));
+#endif
   return Scons(S_integer_time_t(tx), Svector_ref(dtvec, dtvec_nsec));
 }
 
