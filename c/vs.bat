06ce956bd8496e3e968fcf362172ddb3e2859ad4
Bob Burger <burgerrg@gmail.com>
2018-01-03 10:06:17 -0500

    fix c/vs.bat to work on 32-bit Windows
    Original commit: 78f0caf0fbdfa8ff9fd8e4ac1f2b12ee21214fef

--- c/vs.bat
+++ c/vs.bat
@@ -1,22 +1,26 @@
 @echo off
+set Applications=%ProgramFiles(x86)%
+if not "%Applications%" == "" goto win64
+set Applications=%ProgramFiles%
+:win64
 
 :: Set up Visual Studio command line environment variables given a
 :: machine type, e.g., amd64 or x86.
 
 :: Visual Studio 2017 Enterprise
-set BATDIR=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build
+set BATDIR=%Applications%\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build
 if exist "%BATDIR%\vcvarsall.bat" goto found
 
 :: Visual Studio 2017 Professional
-set BATDIR=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build
+set BATDIR=%Applications%\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build
 if exist "%BATDIR%\vcvarsall.bat" goto found
 
 :: Visual Studio 2017 Community
-set BATDIR=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build
+set BATDIR=%Applications%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build
 if exist "%BATDIR%\vcvarsall.bat" goto found
 
 :: Visual Studio 2017 BuildTools
-set BATDIR=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build
+set BATDIR=%Applications%\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build
 if exist "%BATDIR%\vcvarsall.bat" goto found
 
 :: Visual Studio 2015
