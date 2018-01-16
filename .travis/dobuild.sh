f20d7533e8beaaa4cc6fc7183ed70dc7f610048b
Andy Keep <andy.keep@gmail.com>
2018-01-15 22:14:58 -0500

    Updated to run the partialx again.
    Original commit: d32395ab408c09a4ff02d741e1854fbdb92201b8

--- .travis/dobuild.sh
+++ .travis/dobuild.sh
@@ -2,7 +2,7 @@
 
 ./configure -m=$TARGET_MACHINE
 make
-( cd ${TARGET_MACHINE}/mats && make all 2>&1 ) | tee Make.out | grep '^matting '
+( cd ${TARGET_MACHINE}/mats && make partialx 2>&1 ) | tee Make.out | grep '^matting '
 echo "exit code: $?"
 cat ${TARGET_MACHINE}/mats/summary
 diff .travis/summary ${TARGET_MACHINE}/mats/summary
