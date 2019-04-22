1ef5aa0bb0f264bf7f3d22c985b62e5646efab9b
Bob Burger <burgerrg@gmail.com>
2019-04-22 17:53:26 -0400

    add Windows builds and update Linux dist to xenial
    Original commit: e44a209f27f8670c23a1d402b3f968eaa10cad29

---
+++ .travis/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+runmats() {
+  echo travis_fold:start:mats
+  echo make allxhelp "$@"
+  make -C ${TARGET_MACHINE}/mats allxhelp "$@" 2>&1 | tee -a Make.out | grep '^matting '
+  echo travis_fold:end:mats
+}
+
+# Split these out so that we get output every 10 minutes on Windows builds.
+runmats o=0
+runmats o=3
+runmats o=3 cp0=t
+runmats o=3 cp0=t eval=interpret
+
+if [ -f ${TARGET_MACHINE}/mats/summary ]; then
+  cat ${TARGET_MACHINE}/mats/summary
+  diff -q .travis/summary ${TARGET_MACHINE}/mats/summary
+  exit $?
+else
+  exit 1
+fi
