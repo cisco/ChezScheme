ef0bbd6a8ce985f06501b0518ecf28fc409a685c
Gustavo Massaccesi <gustavo@oma.org.ar>
2019-06-02 09:03:09 -0300

    make bootfiles in travis-ci
    Original commit: racket/ChezScheme@dea72f44a0b5bb00c2af77e94a762f2ffcd77e8f

---
+++ .travis/dobootfile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -v
+
+cd ..
+
+case $TARGET_MACHINE in
+  *i3le|ti3le) INST=i386;;
+  *a6le|ta6le) INST=x86_64;;
+  *)
+esac
+curl -L -o installer.sh http://www.cs.utah.edu/plt/snapshots/current/installers/min-racket-current-${INST}-linux-precise.sh
+sh installer.sh --in-place --dest ~/racket/
+
+~/racket/bin/racket -v
+~/racket/bin/raco pkg install -i --auto --no-setup cs-bootstrap
+~/racket/bin/raco setup -D cs-bootstrap
+
+cd ChezScheme/
+
+export MACH=$TARGET_MACHINE
+~/racket/bin/racket -l cs-bootstrap
+
