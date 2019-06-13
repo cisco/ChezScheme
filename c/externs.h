02c848c080cfe7d3a0cebb43b663755c19d15d95
Alexander B. McLin <alex.mclin@gmail.com>
2019-06-13 18:23:02 +0000

    Annotate several function prototypes with NORETURN.
    When functions that do not return are used in switch cases or
    in other certain positions, later versions of gcc such as 7+ or
    above may generate spurious implicit-fallthrough or uninitialized
    variables warnings since gcc does not know those functions don't
    actually return. The spurious warnings stop compilation due to
    the -Werror flag.
    
    Also actual uses of fallthrough in switch cases trigger
    implicit fallthrough warnings when -Wextra flag is on in gcc 7+ or
    above. Those warnings can be suppressed by using /* fall through */
    comments so in some places existing comments were edited to use
    /* fall through */

--- c/externs.h
+++ c/externs.h
@@ -45,6 +45,18 @@ off64_t lseek64(int,off64_t,int);
 #include <setjmp.h>
 #endif
 
+#ifndef NORETURN
+
+#if defined(__GNUC__) || defined(__clang__)
+#define NORETURN __attribute__((noreturn))
+#elif defined(_MSC_VER)
+#define NORETURN __declspec(noreturn)
+#else
+#define NORETURN
+#endif /* defined(__GNUC__) || defined(__clang__) */
+
+#endif /* NORETURN */
+
 /* external procedure declarations */
 /* prototypes gen. by ProtoGen Version 0.31 (Haydn Huntley) 1/18/93 */
 
@@ -304,14 +316,14 @@ extern void S_handle_overflow PROTO((void));
 extern void S_handle_overflood PROTO((void));
 extern void S_handle_apply_overflood PROTO((void));
 extern void S_overflow PROTO((ptr tc, iptr frame_request));
-extern void S_error_reset PROTO((const char *s));
-extern void S_error_abort PROTO((const char *s));
-extern void S_abnormal_exit PROTO((void));
-extern void S_error PROTO((const char *who, const char *s));
-extern void S_error1 PROTO((const char *who, const char *s, ptr x));
-extern void S_error2 PROTO((const char *who, const char *s, ptr x, ptr y));
-extern void S_error3 PROTO((const char *who, const char *s, ptr x, ptr y, ptr z));
-extern void S_boot_error PROTO((const ptr who, ptr s, ptr args));
+extern void S_error_reset PROTO((const char *s)) NORETURN;
+extern void S_error_abort PROTO((const char *s)) NORETURN;
+extern void S_abnormal_exit PROTO((void)) NORETURN;
+extern void S_error PROTO((const char *who, const char *s)) NORETURN;
+extern void S_error1 PROTO((const char *who, const char *s, ptr x)) NORETURN;
+extern void S_error2 PROTO((const char *who, const char *s, ptr x, ptr y)) NORETURN;
+extern void S_error3 PROTO((const char *who, const char *s, ptr x, ptr y, ptr z)) NORETURN;
+extern void S_boot_error PROTO((const ptr who, ptr s, ptr args)) NORETURN;
 extern void S_handle_docall_error PROTO((void));
 extern void S_handle_arg_error PROTO((void));
 extern void S_handle_nonprocedure_symbol PROTO((void));
@@ -319,7 +331,7 @@ extern void S_handle_values_error PROTO((void));
 extern void S_handle_mvlet_error PROTO((void));
 extern void S_register_scheme_signal PROTO((iptr sig));
 extern void S_fire_collector PROTO((void));
-extern void S_noncontinuable_interrupt PROTO((void));
+extern void S_noncontinuable_interrupt PROTO((void)) NORETURN;
 extern void S_schsig_init PROTO((void));
 #ifdef DEFINE_MATHERR
 #include <math.h>
