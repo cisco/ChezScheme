cisco/ChezScheme

commit 72d90e4c67849908da900d0b6249a1dedb5f8c7f

library-manager, numeric, and bytevector-compres improvements - added invoke-library     syntax.ss, primdata.ss,     8.ms, root-experr*,     libraries.stex, release_notes.stex - updated the date     release_notes.stex - libraries contained within a whole program or library are now   marked pending before their invoke code is run so that invoke   cycles are reported as such rather than as attempts to invoke   while still loading.     compile.ss, syntax.ss, primdata.ss,     7.ms, root-experr* - the library manager now protects against unbound references   from separately compiled libraries or programs to identifiers   ostensibly but not actually exported by (invisible) libraries   that exist only locally within a whole program.  this is done by   marking the invisibility of the library in the library-info and   propagating it to libdesc records; the latter is checked upon   library import, visit, and invoke as well as by verify-loadability.   the import and visit code of each invisible no longer complains   about invisibility since it shouldn't be reachable.     syntax.ss, compile.ss, expand-lang.ss,     7.ms, 8.ms, root-experr*, patch* - documented that compile-whole-xxx's linearization of the   library initialization code based on static dependencies might   not work for dynamic dependencies.     system.stex - optimized bignum right shifts so the code (1) doesn't look at   shifted-off bigits if the bignum is positive, since it doesn't   need to know in that case if any bits are set; (2) doesn't look   at shifted-off bigits if the bignum is negative if it determines   that at least one bit is set in the bits shifted off the low-order   partially retained bigit; (3) quits looking, if it must look, for   one bits as soon as it finds one; (4) looks from both ends under   the assumption that set bits, if any, are most likely to be found   toward the high or low end of the bignum rather than just in the   middle; and (5) doesn't copy the retained bigits and then shift;

c/schsig.c
@@ -501,15 +501,23 @@ void S_noncontinuable_interrupt() {
 }
 
 #ifdef WIN32
+ptr S_dequeue_scheme_signals(ptr tc) {
+  return Snil;
+}
+
+ptr S_allocate_scheme_signal_queue() {
+  return (ptr)0;
+}
+
+void S_register_scheme_signal(sig) iptr sig; {
+  S_error("register_scheme_signal", "unsupported in this version");
+}
+
 /* code courtesy Bob Burger, burgerrg@sagian.com
    We cannot call noncontinuable_interrupt, because we are not allowed
    to perform a longjmp inside a signal handler; instead, we don't
    handle the signal, which will cause the process to terminate.
 */
-
-void S_register_scheme_signal(sig) iptr sig; {
-    S_error("register_scheme_signal", "unsupported in this version");
-}
 
 static BOOL WINAPI handle_signal(DWORD dwCtrlType) {
   switch (dwCtrlType) {
@@ -538,6 +546,8 @@ static void init_signal_handlers() {
 #include <signal.h>
 
 static void handle_signal PROTO((INT sig, siginfo_t *si, void *data));
+static IBOOL enqueue_scheme_signal PROTO((ptr tc, INT sig));
+static ptr allocate_scheme_signal_queue PROTO((void));
 static void forward_signal_to_scheme PROTO((INT sig));
 
 #define RESET_SIGNAL {\
@@ -547,18 +557,88 @@ static void forward_signal_to_scheme PROTO((INT sig));
     sigprocmask(SIG_UNBLOCK,&set,(sigset_t *)0);\
 }
 
+/* we buffer up to SIGNALQUEUESIZE - 1 unhandled signals, the start dropping them. */
+#define SIGNALQUEUESIZE 64
+static IBOOL scheme_signals_registered;
+
+/* we use a simple queue for pending signals.  signals are enqueued only by the
+   C signal handler and dequeued only by the Scheme event handler.  since the signal
+   handler and event handler run in the same thread, there's no need for locks
+   or write barriers. */
+
+struct signal_queue {
+  INT head;
+  INT tail;
+  INT data[SIGNALQUEUESIZE];
+};
+
+static IBOOL enqueue_scheme_signal(ptr tc, INT sig) {
+  struct signal_queue *queue = (struct signal_queue *)(SIGNALINTERRUPTQUEUE(tc));
+  /* ignore the signal if we failed to allocate the queue */
+  if (queue == NULL) return 0;
+  INT tail = queue->tail;
+  INT next_tail = tail + 1;
+  if (next_tail == SIGNALQUEUESIZE) next_tail = 0;
+  /* ignore the signal if the queue is full */
+  if (next_tail == queue->head) return 0;
+  queue->data[tail] = sig;
+  queue->tail = next_tail;
+  return 1;
+}
+
+ptr S_dequeue_scheme_signals(ptr tc) {
+  ptr ls = Snil;
+  struct signal_queue *queue = (struct signal_queue *)(SIGNALINTERRUPTQUEUE(tc));
+  if (queue == NULL) return ls;
+  INT head = queue->head;
+  INT tail = queue->tail;
+  INT i = tail;
+  while (i != head) {
+    if (i == 0) i = SIGNALQUEUESIZE;
+    i -= 1;
+    ls = Scons(Sfixnum(queue->data[i]), ls);
+  }
+  queue->head = tail;
+  return ls;
+}
+
 static void forward_signal_to_scheme(sig) INT sig; {
   ptr tc = get_thread_context();
 
-  SIGNALINTERRUPTPENDING(tc) = Sfixnum(sig);
-  SOMETHINGPENDING(tc) = Strue;
+  if (enqueue_scheme_signal(tc, sig)) {
+    SIGNALINTERRUPTPENDING(tc) = Strue;
+    SOMETHINGPENDING(tc) = Strue;
+  }
   RESET_SIGNAL
+}
+
+static ptr allocate_scheme_signal_queue() {
+  /* silently fail to allocate space for signals if malloc returns NULL */
+  struct signal_queue *queue = malloc(sizeof(struct signal_queue));
+  if (queue != (struct signal_queue *)0) {
+    queue->head = queue->tail = 0;
+  }
+  return (ptr)queue;
+}
+
+ptr S_allocate_scheme_signal_queue() {
+  return scheme_signals_registered ? allocate_scheme_signal_queue() : (ptr)0;
 }
 
 void S_register_scheme_signal(sig) iptr sig; {
     struct sigaction act;
 
-    sigemptyset(&act.sa_mask);
+    tc_mutex_acquire()
+    if (!scheme_signals_registered) {
+      ptr ls;
+      scheme_signals_registered = 1;
+      for (ls = S_threads; ls != Snil; ls = Scdr(ls)) {
+        SIGNALINTERRUPTQUEUE(THREADTC(Scar(ls))) = S_allocate_scheme_signal_queue();
+      }
+    }
+    tc_mutex_release()
+
+    sigfillset(&act.sa_mask);
     act.sa_flags = 0;
     act.sa_handler = forward_signal_to_scheme;
     sigaction(sig, &act, (struct sigaction *)0);
@@ -690,6 +770,7 @@ void S_schsig_init() {
 
         S_protect(&S_G.error_id);
         S_G.error_id = S_intern((const unsigned char *)"$c-error");
+        scheme_signals_registered = 0;
     }
 
 

commit f410e7c0163195ca434e501bb36287091a351403

Exclude unresolved symbol scheme_signals_registered on Win32 builds
Commit 72d90e4 ("library-manager, numeric, and bytevector-compres
improvements") introduced a regression causing Win32 nmake builds to
fail due to an undefined symbol in c\schsig.c.

This symbol (scheme_signals_registered) is defined in a preprocessor-
conditional code block excluding WIN32 between lines 544 and 737.

This commit bypasses the build regression by enclosing the unresolved
expression in a preprocessor conditional excluding WIN32.

See GitHub Issue #497 for more details:
https://github.com/cisco/ChezScheme/issues/497

c/schsig.c
@@ -770,7 +770,9 @@ void S_schsig_init() {
 
         S_protect(&S_G.error_id);
         S_G.error_id = S_intern((const unsigned char *)"$c-error");
+#ifndef WIN32
         scheme_signals_registered = 0;
+#endif
     }
 
 