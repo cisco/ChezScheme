f302495700446cb062cd29f8a55f12f7edaa90d8
Matthew Flatt <mflatt@racket-lang.org>
2020-11-29 15:09:05 -0700

    repair pb code endian swap
    Original commit: racket/ChezScheme@a06850d19d531bc01d21c2883288c3741ba7756a

--- c/fasl.c
+++ c/fasl.c
@@ -1968,7 +1968,7 @@ static void swap_code_endian(octet *code, uptr len)
 
       if (a == pb_adr) {
         /* delta can be negative for a mvlet-error reinstall of the return address */
-        iptr delta = (int16_t)(uint16_t)(((uptr)d << 16) + c);
+        iptr delta = (int16_t)(uint16_t)(((uptr)d << 8) + c);
         if (delta > 0) {
           /* after a few more instructions, we'll hit
              a header where 64-bit values needs to be
