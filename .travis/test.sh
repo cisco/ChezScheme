4e60d948273f8641eca9a18104192d3f18742244
Jamie Taylor <Jamie.Taylor@pobox.com>
2021-04-21 22:28:27 -0400

    increase travis timeout; more log output on failure

--- .travis/test.sh
+++ .travis/test.sh
@@ -6,6 +6,10 @@ if [ -f "$TARGET_MACHINE"/mats/summary ]; then
   diff -q .travis/summary "$TARGET_MACHINE"/mats/summary
   rc=$?
   if [ $rc -gt 0 ]; then
+      echo 'travis_fold:start:make_prereqs_output'
+      echo "Make output from $TARGET_MACHINE/mats/Make.out:"
+      cat "$TARGET_MACHINE"/mats/Make.out
+      echo 'travis_fold:end:make_prereqs_output'
       for log in "$TARGET_MACHINE"/mats/output*/Make.out ; do
           echo 'travis_fold:start:make_output'
           echo "Make output from ${log}:"
