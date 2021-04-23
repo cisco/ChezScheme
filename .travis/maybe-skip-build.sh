88c2051c4f869786a70923cd47b41e4dc372f9a5
Jamie Taylor <Jamie.Taylor@pobox.com>
2021-04-23 19:46:12 -0400

    debug output to see what TRAVIS_COMMIT_MESSAGE really is
    travis:only: a6nt i3nt

--- .travis/maybe-skip-build.sh
+++ .travis/maybe-skip-build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Checking to see whether to skip build for $TARGET_MACHINE due to commit message"
+echo "$TRAVIS_COMMIT_MESSAGE"
 if echo "$TRAVIS_COMMIT_MESSAGE" | grep -q "^travis:only:" ; then
     if ! echo "$TRAVIS_COMMIT_MESSAGE" | grep "^travis:only:" | grep -q "\<${TARGET_MACHINE}\>" ; then
         echo "Skipping builds for target machine type $TARGET_MACHINE due to commit message:"
