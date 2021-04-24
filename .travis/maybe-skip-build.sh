d731f3eaf768d0f9c15551039440ee30fde4e5c3
Jamie Taylor <Jamie.Taylor@pobox.com>
2021-04-23 20:07:58 -0400

    Additional travis debugging output travis:only:a6nt

--- .travis/maybe-skip-build.sh
+++ .travis/maybe-skip-build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+git branch -v
+echo "TRAVIS_COMMIT: $TRAVIS_COMMIT"
+echo "TRAVIS_COMMIT_RANGE: $TRAVIS_COMMIT_RANGE"
+echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 echo "Checking to see whether to skip build for $TARGET_MACHINE due to commit message"
 echo "$TRAVIS_COMMIT_MESSAGE"
-if echo "$TRAVIS_COMMIT_MESSAGE" | grep -q "^travis:only:" ; then
-    if ! echo "$TRAVIS_COMMIT_MESSAGE" | grep "^travis:only:" | grep -q "\<${TARGET_MACHINE}\>" ; then
+msg_only="$(echo "$TRAVIS_COMMIT_MESSAGE" | grep "^travis:only:")"
+if test -n "$msg_only" ; then
+    if ! echo "$msg_only" | grep -q "\<${TARGET_MACHINE}\>" ; then
         echo "Skipping builds for target machine type $TARGET_MACHINE due to commit message:"
-        echo "$TRAVIS_COMMIT_MESSAGE" | grep "^travis:only:"
+        echo "$msg_only"
         exit 1
     fi
 fi
