96ee63d19e2c6dd46a00c7fc83fb307a5ec537a7
Jamie Taylor <jltaylor-us@users.noreply.github.com>
2021-05-01 20:08:27 -0400

    run mats configurations in parallel (#561)
    * refactor mats to allow different configurations to run in parallel.
    
    The {partial,all,bully}x targets now support being run in parallel should
    make decide to do so (e.g., via the -j flag)
    
    * fix mats ignoring "rmg" parameter
    
    * Update travis-ci build scripts to use new partialx target and run multiple
    jobs in parallel, based on the number of processors available.
    
    * Add a way to only run particular machines in travis-ci by including
    a line that starts with "travis:only:" and lists the machine types in the
    commit message.

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-runmats() {
-  echo travis_fold:start:mats
-  echo make allxhelp "$@"
-  make -C "$TARGET_MACHINE"/mats allxhelp "$@" 2>&1 | .travis/matting.sh
-  echo travis_fold:end:mats
-}
 
-runmats o=0
-runmats o=3
-runmats o=3 cp0=t
-runmats o=3 cp0=t eval=interpret
+if test -n "$PARALLEL_MATS" ; then
+    njobs="$PARALLEL_MATS"
+else
+    njobs="$(getconf _NPROCESSORS_ONLN)"
+fi
+make -C "$TARGET_MACHINE"/mats -j "$njobs" partialx
 
 if [ -f "$TARGET_MACHINE"/mats/summary ]; then
-  cat "$TARGET_MACHINE"/mats/summary
   diff -q .travis/summary "$TARGET_MACHINE"/mats/summary
-  exit $?
+  rc=$?
+  if [ $rc -gt 0 ]; then
+      echo 'travis_fold:start:make_prereqs_output'
+      echo "Make output from $TARGET_MACHINE/mats/Make.out:"
+      cat "$TARGET_MACHINE"/mats/Make.out
+      echo 'travis_fold:end:make_prereqs_output'
+      for log in "$TARGET_MACHINE"/mats/output*/Make.out ; do
+          echo 'travis_fold:start:make_output'
+          echo "Make output from ${log}:"
+          cat "${log}"
+          echo 'travis_fold:end:make_output'
+      done
+  fi
+  exit $rc
 else
   exit 1
 fi
