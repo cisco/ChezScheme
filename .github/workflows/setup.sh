c4645021292aa60ee81b8dae5d15a665b4bf2a94
Bob Burger <bburger@indigobio.com>
2021-06-25 15:39:32 -0400

    clean up line endings in Windows

---
+++ .github/workflows/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e -o pipefail
+case "$TARGET_MACHINE" in
+  *i3le)
+    sudo dpkg --add-architecture i386
+    sudo apt-get update
+    sudo apt-get install gcc-multilib lib32ncurses5-dev uuid-dev:i386
+    ;;
+  *nt)
+    # Use proper line endings
+    git config core.autocrlf false
+    git reset --hard
+    ;;
+esac
