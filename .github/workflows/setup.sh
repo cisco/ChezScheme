c7a57b9054cef0341e0f95f80aa99c8ef9b26ce8
Bob Burger <bburger@indigobio.com>
2021-06-25 19:24:30 -0400

    clean up

--- .github/workflows/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e -o pipefail
-case "$TARGET_MACHINE" in
-  *i3le)
-    sudo dpkg --add-architecture i386
-    sudo apt-get update
-    sudo apt-get install gcc-multilib lib32ncurses5-dev uuid-dev:i386
-    ;;
-esac
