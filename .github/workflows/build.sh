999772a309df4fb2f65af6a0196e378683bdbe79
Matthew Flatt <mflatt@racket-lang.org>
2022-09-20 13:45:56 +0800

    adapt merged GitHub Actions configuration
    Original commit: racket/ChezScheme@14e46941755dbdad61486e6e5cb88537c3fc92a0

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -e -o pipefail
-./configure -m="$TARGET_MACHINE"
-make -j $(getconf _NPROCESSORS_ONLN)
+export ZUO_JOBS="$(getconf _NPROCESSORS_ONLN)"
+if test "$TOOLCHAIN" = vs ; then
+    cmd.exe /c "build.bat $TARGET_MACHINE"
+else
+    if test -n "$CONFIGURE_ARGS" ; then
+        ./configure $CONFIGURE_ARGS
+    else
+        ./configure -m="$TARGET_MACHINE"
+    fi
+    make
+fi
 case "$TARGET_MACHINE" in
   *a6nt)
     curl -Ls https://github.com/burgerrg/win-iconv/releases/download/v0.0.9/iconv-x64.dll > "$TARGET_MACHINE"/bin/"$TARGET_MACHINE"/iconv.dll
--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-if test -n "$PARALLEL_MATS" ; then
-    njobs="$PARALLEL_MATS"
-else
-    njobs="$(getconf _NPROCESSORS_ONLN)"
+export ZUO_JOBS="$(getconf _NPROCESSORS_ONLN)"
+if test "$TEST_TARGET" = ""; then
+    TEST_TARGET=test-some
 fi
-make -C "$TARGET_MACHINE"/mats -j "$njobs" partialx
+if test "$TOOLCHAIN" = vs ; then
+    cmd.exe /c "build.bat $TARGET_MACHINE /$TEST_TARGET"
+else
+    make $TEST_TARGET
+fi
