01b5bb60ccc2364e49a5aae5d16aa56e03b3ab37
Matthew Flatt <mflatt@racket-lang.org>
2023-07-14 08:28:20 -0600

    CI repair for Windows
    Running "cmd /c" from bash no longer seems to work in a GitHub action.
    I'm not sure why, but run the build instead through a separate step
    that uses `shell: cmd`.
    
    Original commit: racket/ChezScheme@9922a1ea3d31deaa5963050fb8d41dae92ef68b0

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -2,7 +2,8 @@
 set -e -o pipefail
 export ZUO_JOBS="$(getconf _NPROCESSORS_ONLN)"
 if test "$TOOLCHAIN" = vs ; then
-    cmd.exe /c "build.bat $TARGET_MACHINE"
+    # cmd.exe /c "build.bat $TARGET_MACHINE"
+    echo assuming built previously
 else
     if test -n "$CONFIGURE_ARGS" ; then
         ./configure $CONFIGURE_ARGS
