b54a1788e1cd7f0bc35a945df5f4de552a53725f
Matthew Flatt <mflatt@racket-lang.org>
2023-09-01 17:33:53 -0600

    normalize references to NetBSD headers and libraries
    Original commit: racket/ChezScheme@73b4ec3c21207a72d1e9cafee9cfb8e6c2e7d3e3

--- c/expeditor.c
+++ c/expeditor.c
@@ -660,9 +660,6 @@ static void s_ee_set_color(int color_id, IBOOL background) {
 # define CHTYPE int
 # include </usr/include/curses.h>
 # include </usr/include/term.h>
-#elif defined(NETBSD)
-# include <ncurses.h>
-# include <ncurses/term.h>
 #elif defined(__DragonFly__)
 # include <ncurses/curses.h>
 # include <ncurses/term.h>
