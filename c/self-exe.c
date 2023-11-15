91ffeba76759fbcb0390664ede166360d0222887
Matthew Flatt <mflatt@racket-lang.org>
2023-11-15 06:47:45 -0700

    add license header to "self-exe.c"

--- c/self-exe.c
+++ c/self-exe.c
@@ -1,3 +1,19 @@
+/* self-exe.c
+ * Copyright 1984-2017 Cisco Systems, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /*
   This file is meant to be standalone and suitable for use in
   programs other than Chez Scheme.
@@ -22,7 +38,7 @@
     print the result from `S_get_process_executable_path`, which is
     useful for testing.
 
-  Parts of the implementaiton here are from the LLVM Project under
+  Parts of the implementation here are from the LLVM Project under
   the Apache License v2.0 with LLVM Exceptions.
 */
 
