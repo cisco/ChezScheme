5b14db5d3d774f9d99c2b7ee706dd763ccc07e7e
Matthew Flatt <mflatt@racket-lang.org>
2025-02-17 14:12:10 -0700

    building with MinGW: link to MSVCRT/UCRT for `pow`, `sin`, and `cos`

--- c/prim5.c
+++ c/prim5.c
@@ -1472,25 +1472,20 @@ static double s_pow(double x, double y) { return pow(x, y); }
 #endif /* i3fb/ti3fb */
 
 #ifdef __MINGW32__
-/* cos() and sin() do not handle large values nicely,
-   so use fmod() to get reasonably close */
-# define INTO_SINCOS_RANGE(x) (((x > 1e9) || (x < -1e9)) \
-			       ? fmod(x, 2*atan2(0.0, -1.0)) \
-			       : x)
 /* asinh() and atanh() sometimes get zero sign wrong */
 # define CHECK_ASINTAN_ZERO(x, e) ((x == 0.0) \
                                    ? (signbit(x) ? -0.0 : 0.0)  \
                                    : e)
+/* see also "mingw.zuo" */
 #else
-# define INTO_SINCOS_RANGE(x) x
 # define CHECK_ASINTAN_ZERO(x, e) e
 #endif
 
 static double s_sqrt(double x) { return sqrt(x); }
 
-static double s_sin(double x) { return sin(INTO_SINCOS_RANGE(x)); }
+static double s_sin(double x) { return sin(x); }
 
-static double s_cos(double x) { return cos(INTO_SINCOS_RANGE(x)); }
+static double s_cos(double x) { return cos(x); }
 
 static double s_tan(double x) { return tan(x); }
 
